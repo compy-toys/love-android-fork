ba5397b229fb85963d983553a13893f637edb034
aldum <aldum@artixlinux.org>
2025-09-18 13:57:49 +0200

    refactor: translate ProjectSelector to Kotlin

---
+++ app/src/main/kotlin/org/love2d/android/ProjectSelector.kt
@@ -0,0 +1,27 @@
+package org.love2d.android
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts.OpenDocument
+import androidx.appcompat.app.AppCompatActivity
+
+class ProjectSelector : AppCompatActivity() {
+  private val projectPicker = registerForActivityResult(OpenDocument()) { uri ->
+    uri?.let {
+      val intent = Intent(this, CompyActivity::class.java)
+      intent.setData(uri)
+      intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+      startActivity(intent)
+    } ?: run {
+
+    }
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    projectPicker.launch(arrayOf("*/*"))
+  }
+}
